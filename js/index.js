b89a82e875c7289266f28ba1ea8d74b724f4f7b4
unknown <Aldana Pacheco Martinez>
2023-05-05 13:02:23 -0300

    Rama dev lista para pasar a main

--- js/index.js
+++ js/index.js
@@ -22,9 +22,9 @@ if (!products || products.length===0) {
         price: "2500",
         resume:
             "Para disfrutar de su luz y aroma a vainilla, esta vela de soja ha sido elaborada artesanalmente cuidando sus detalles para hacerla única. En ella se busca reflejar, como su nombre lo indica, toda la magia del mar.",
-        description: "",
+        description: "Creada para despertarnos esa sensación de calidez y paz, propio del mar. Disponible en aroma: vainilla, manzana y canela y flores blancas. Thálassa es definitivamente de las más buscadas. Cuenta con un pabilo que llega hasta su base para un uso parejo, y sus medidas son 25cm x 10cm.",
         thumbnail: "/assets/images/sea-candle.png",
-        images: ["/assets/images/arch-candle.png"],
+        images: ["/assets/details-images/sea-candle1.jpeg", "/assets/details-images/sea-candle2.jpeg"],
         category: "velas",
         stock: 10,
         favorite:true,
@@ -38,9 +38,9 @@ if (!products || products.length===0) {
             price: "2700",
             resume:
             "Con aroma a coco, prepara y perfuma el ambiente para crear momentos de calma, armonía y bienestar. De diseño fluido y gran durabilidad, esta vela de cera vegetal es la compañía perfecta para cualquier espacio.",
-            description: "",
+            description: "Effradeia es única por sus detalles. Cuenta con aroma a coco y su pabilo 100% ecológico llega hasta su base. Exclusiva y refinada. Sus medidas son 20cm x 15cm.",
             thumbnail: "/assets/images/shapes-candle.png",
-            images: ["/assets/images/arch-candle.png"],
+            images: ["/assets/details-images/effradeia2.jpeg","/assets/details-images/effradeia1.jpeg"],
             category: "velas",
             stock: 5,
             favorite:false,
@@ -52,9 +52,9 @@ if (!products || products.length===0) {
             price: "3000",
             resume:
             "Llamada calidez, este diseño presenta notas amaderadas y florales. Ideal para crear esos instantes de encuentro con uno mismo y disfrutar de su luz. También disponible en su versión mango y maracuyá.",
-            description: "",
+            description: "Las palabras están de más, cuando su diseño lo dice todo. Sus curvas buscan dar una idea de fluidez y movimiento. Disponible en aroma lima y verbena y mango y maracuyá. Frutal y fresca. Sus medidas son 15cm x 10cm aproximadamente.",
             thumbnail: "/assets/images/wind-candle.png",
-            images: ["/assets/images/arch-candle.png"],
+            images: ["/assets/details-images/zestasia1.jpeg", "/assets/details-images/zestasia2.jpeg"],
             category: "velas",
             stock: 3,
             favorite:true,
@@ -66,9 +66,9 @@ if (!products || products.length===0) {
             price: "3500",
             resume:
             "Con doble pabilo 100% ecológico para que su consumo se de parejamente, desea brindar ese detalle minimalista que embellece cualquier espacio. Cuenta con aroma a peonias y flores blancas, una fragancia clásica, fresca y elegante.",
-            description: "",
+            description: "De nuestras velas más grandes y duraderas, llega Ciclos, con doble pabilo 100% ecológico y confeccionada en cera vegetal cuidando que mantenga su figura durante todo el consumo. Sus medidas son 35cm x 25cm.",
             thumbnail: "/assets/images/circle-candle.png",
-            images: ["/assets/images/arch-candle.png"],
+            images: ["/assets/details-images/ciclos1.jpeg", "/assets/details-images/ciclos2.png"],
             category: "velas",
             stock: 8,
             favorite:false,
@@ -80,9 +80,9 @@ if (!products || products.length===0) {
             price: "3000",
             resume:
             "Elaborada en cera vegetal, se conjuga en uno de nuestros diseños mas complejos. Confeccionada 100% a mano y con esencias que dejarán un suave aroma a frambuesas. Compañera ideal para momentos especiales.",
-            description: "",
+            description: "De diseño complejo y que simula un entrelazado infinito. Gran detalle para cualquier espacio. Disponible en sus fragancias garndenia, lavanda y frambuesas. Las medidas son: 20cm x 12cm",
             thumbnail: "/assets/images/knot-candle.png",
-            images: ["/assets/images/arch-candle.png"],
+            images: ["/assets/details-images/gravates1.jpeg", "/assets/details-images/gravates2.jpeg"],
             category: "velas",
             stock: 3,
             favorite:false,
@@ -94,9 +94,9 @@ if (!products || products.length===0) {
             price: "2500",
             resume:
             "Fomada por líneas que simulan suaves movimientos, este diseño en cera de soja cuenta con olor a café y caramelo. Destacada tanto por su fragancia como por su originalidad. Disfrutala también en aroma naranja y canela.",
-            description: "",
+            description: "Su figura asimétrica brinda un toque especial a sus lineas, diseñadas para combinar a la perfección. Sumamente solicitada por su aroma a café y caramelo. Las medidas son: 18cm x 12cm. ",
             thumbnail: "/assets/images/large.candle.png",
-            images: ["/assets/images/arch-candle.png"],
+            images: ["/assets/details-images/diumos1.png", "/assets/details-images/diumos2.jpeg"],
             category: "velas",
             stock: 12,
             favorite:false,
@@ -108,9 +108,9 @@ if (!products || products.length===0) {
             price: "3200",
             resume:
             "Con su versatilidad busca reflejar el viento. Está confeccionada en cera de soja, con aceites aromáticos de verbena y limón. Vertida y terminada a mano, cuidando cada uno de sus detalles.",
-            description: "",
+            description: "Trabajada en detalle, viene también en su otra versión. Ambas disponibles con sus esencias aromáticas de incienso, madera de cedro y canela y naranja. Sus medidas son aproximadamente: 20cm x 10cm. ",
             thumbnail: "/assets/images/feather-candle.png",
-            images: ["/assets/images/arch-candle.png"],
+            images: ["/assets/details-images/aeraki1.jpeg", "/assets/details-images/aeraki2.jpeg"],
             category: "velas",
             stock: 4,
             favorite:true,
@@ -122,9 +122,9 @@ if (!products || products.length===0) {
             price: "3000",
             resume:
             "Trabajada artesanalmente y con estilo bombé, esta vela cuenta con aroma a té blanco y jengibre. Innovadora y delicada, es realizada con un pabilo de algodón 100% ecológico al igual que el resto de nuestros productos.",
-            description: "",
+            description: "Zei simula ser una gota de agua, pero trabajada en detalle. De lineas definidas, que le brindan un acabado minimalista. Disponible también en su versión large, una dupla que no puede faltar.",
             thumbnail: "/assets/images/lines-candle.png",
-            images: ["/assets/images/arch-candle.png"],
+            images: ["/assets/details-images/zei1.jpeg", "/assets/details-images/zei2.jpeg"],
             category: "velas",
             stock: 1,
             favorite:true,
@@ -136,9 +136,9 @@ if (!products || products.length===0) {
             price: "2800",
             resume:
             "Siendo la combinación perfecta para decorar y perfumar, llega esta vela con su aroma a lavanda y tilo. Una fragancia que vamos a desear permanezca mucho tiempo. Incluye dos pabilos, para que al utilizarla no pierda su figura.",
-            description: "",
+            description: "Iris es ese momento de relajación que sentimos al contemplar su luz. Su esencia aromática es ideal para crear momentos de descanso y renovación. También disponible en color oliva. Sus medidas son 25cm x 20cm",
             thumbnail: "/assets/images/arch-candle.png",
-            images: ["/assets/images/arch-candle.png"],
+            images: ["/assets/details-images/arch1.jpeg", "/assets/details-images/arch2.png"],
             category: "velas",
             stock: 11,
             favorite:false,
@@ -150,9 +150,9 @@ if (!products || products.length===0) {
             price: "2200",
             resume:
             "Inspirada en el agua y con suaves notas de manzana y canela, esta vela conjuga delicadeza y sencillez. Combinable con cualquier ambiente y decoración. Disfrutala también en su versión vainilla.",
-            description: "",
+            description: "Su sencillez combina con todo. Disponible también en aroma vainilla y lavanda. Confeccionada en cera de soja y con pabilo 100% vegetal. Sus medidas son 11cm x 7cm",
             thumbnail: "/assets/images/water-candle.png",
-            images: ["/assets/images/arch-candle.png"],
+            images: ["/assets/details-images/nero1.jpeg", "/assets/details-images/nero2.jpeg"],
             category: "velas",
             stock: 11,
             favorite:false,
@@ -164,9 +164,9 @@ if (!products || products.length===0) {
             price: "9600",
             resume:
             "Tallada a mano en una sola pieza de piedra blanca, esta escultura abstracta se entreteje creando una forma única y fluida. Busca simular un movimiento continuo e infinito. Sus medidas son 30cm de alto y 45cm de ancho.",
-            description: "",
+            description: "Con múltiples usos como el que podemos observar, es protagonista en cualquier espacio. Su diseño se basa en un único movimiento ininterrumpido que se entrelaza, brindando un acabado minimalista y moderno.",
             thumbnail: "/assets/images/sculpture.png",
-            images: ["/assets/images/arch-candle.png"],
+            images: ["/assets/details-images/nix1.png", "/assets/details-images/nix2.jpeg"],
             category: "Esculturas",
             stock: 1,
             favorite:false,
@@ -178,9 +178,9 @@ if (!products || products.length===0) {
             price: "5800",
             resume:
             "Sumándose a la Línea Voltaire de arte y esculturas, llega esta base realizada en cerámica artesanal con acabado semi-laqueado y detalles en su pie. Completamente hecha a mano. Exclusiva y delicada.",
-            description: "",
+            description: "Delicada y multiusos. En ella podemos realizar hermosas combinaciones de velas o depositar cualquier objeto que deseemos como frutas por ejemplo. Un detalle fino y sumamente útil.",
             thumbnail: "/assets/images/deco-dish.png",
-            images: ["/assets/images/arch-candle.png"],
+            images: ["/assets/details-images/helios1.webp", "/assets/details-images/helios2.webp"],
             category: "Esculturas",
             stock: 1,
             favorite:false,
@@ -192,9 +192,9 @@ if (!products || products.length===0) {
             price: "18000",
             resume:
             "Los protagonistas de cualquier habitación seguramente sean los movimientos y texturas de este cuadro, realizado en yeso, con técnicas únicas y contemporáneas. Sus medidas son 40cm de alto y 60cm de ancho. ",
-            description: "",
+            description: "Sumamente volátil y fácil de combinar en cualquier espacio. Aquí podemos ver en detalle su confección, con mucha dedicación, como todo objeto de arte. Una combinación refinada de figuras, arte y fluidez, con mucho para apreciar.",
             thumbnail: "/assets/images/frame-art-2.png",
-            images: ["/assets/images/arch-candle.png"],
+            images: ["/assets/details-images/zadig1.jpeg", "/assets/details-images/zadig2.jpeg"],
             category: "Cuadros",
             stock: 1,
             favorite:false,
@@ -206,9 +206,9 @@ if (!products || products.length===0) {
             price: "25000",
             resume:
             "Otro de los íconos de la Línea Voltaire, es este abstracto realizado en yeso que combina la planitud de un cuadro con el relieve de las esculturas. Sus diferentes niveles, con terminaciones talladas a mano, llegan a formar la esencia del mismo y sus figuras, quedan libres a nuestra interpretación. Sus medidas son 40cm de alto y 45cm de ancho. ",
-            description: "",
+            description: "Nada más para contar que este cuadro no pueda mostrar por sí mismo. Un objeto para apreciar y contemplar. Esta obra exclusiva, se realiza a pedido del cliente.",
             thumbnail: "/assets/images/frame-art.png",
-            images: ["/assets/images/arch-candle.png"],
+            images: ["/assets/details-images/monroe1.jpeg", "/assets/details-images/monroe2.jpeg"],
             category: "Cuadros",
             stock: 1,
             favorite:true,
@@ -264,7 +264,7 @@ function renderizarProductos(products) {
 
 function addToCart(product){
     if(!currentUser){
-        showAlert('Para poder agregar productos a su carro de compras, debe ingresar a su cuenta. Si aún no posee una, puede hacerlo en la seccion REGISTRARSE del menu superior.',null,8000)
+        showAlert('Para poder agregar productos a su carro de compras, debe ingresar a su cuenta. Si aún no posee una, puede hacerlo en la sección REGISTRARSE del menú superior.',null,8000)
         setTimeout(() => {
             window.location.href='/pages/login/login.html'
         }, 8000); 
@@ -302,9 +302,16 @@ function handleBuscarIndex(evt){
         return el.name.toLowerCase().includes(searchValue) ||
         el.category.toLowerCase().includes(searchValue)         
     })
-    document.querySelector('.section-cards__products-count').innerHTML = `Se encontraron ${searchResults.length} productos`
+    if(searchResults.length > 1) {document.querySelector('.section-cards__products-count').innerHTML = `Se encontraron ${searchResults.length} productos`}
+    else{ document.querySelector('.section-cards__products-count').innerHTML = `Se encontró 1 producto`}
+    if(searchResults.length === 0) {document.querySelector('.section-cards__products-count').innerHTML = `No se encontraron productos. Puede buscar con palabras similares.`}
 
     renderizarProductos(searchResults)
+}
+
+function cleanSearchIndex(){
+    renderizarProductos(products)
+    document.getElementById('search-product-index').value=null
 }
 
 
--- js/product-detail.js
+++ js/product-detail.js
@@ -6,13 +6,14 @@ const product = products[indice];
 const productDetail = document.getElementById('product-detail').innerHTML= 
 `
 <div class="container-dtl__carrousel">
-<img src="${product?.thumbnail}" alt="" class="container-dtl__imgs">
+<img src="${product?.images[0]||'/assets/images/logo-marca.png'}" alt="" class="container-dtl__imgs" id='image-product-detail'>
+<img src="${product?.images[1]||'/assets/images/logo-marca.png'}" alt="" class="second-image-detail">
 </div>
 
 <div class="container-description">
 <h3 class="subtitle-dtl">${product?.category}</h3>
 <h1 class="title-dtl">${product?.name}</h1>
-<p class="detalle-dtl">${product?.resume}</p>
+<p class="detalle-dtl">${product?.description}</p>
     <div class="container-description__footer">
         <div class="price-dtl">$${product?.price}</div>
         <div class="stock-dtl">
@@ -25,7 +26,7 @@ const productDetail = document.getElementById('product-detail').innerHTML=
             </div>
     </div>
     <div class="container-btn-comprar">
-<button class="container-description__btn-comprar" onclick='addToCart()'>
+<button class="container-description__btn-comprar" onclick='addToCart(${JSON.stringify(product)})'>
     Lo quiero!
 </button>
 </div>
@@ -34,12 +35,42 @@ const productDetail = document.getElementById('product-detail').innerHTML=
 
 `
 
-function addToCart(){
+function addToCart(product){
     let cantidad=document.getElementById('suma-products-detail').value
-    console.log(cantidad)
+
+    if(!currentUser){
+        showAlert('Para poder agregar productos a su carro de compras, debe ingresar a su cuenta. Si aún no posee una, puede hacerlo en la sección REGISTRARSE del menú superior.',null,8000)
+        setTimeout(() => {
+            window.location.href='/pages/login/login.html'
+        }, 8000); 
+            
+        return
+    }
+
+let userCart=allCarts?.filter((el)=> el?.email === currentUser?.email)||[]
+
+
+    let index = (userCart[0].order.findIndex(el=>el.product==product.id))
+    
+    if(index !== -1){
+        userCart[0].order[index].quantity++ 
+    }else{
+        userCart[0].order.push({
+            product:product.id,
+            quantity:Number(cantidad)
+        })
+    }
+    
+    window.localStorage.setItem('cart',JSON.stringify(userCart))
+    renderOrder(userCart)
+    actualizarBadge(userCart)
+    showAlert('Producto agregado al carro de compras correctamente.','ok')
 }
 
+
 function changeQuantity(operator){
+if(document.getElementById('suma-products-detail').value==='0' && operator==='-'){return}
+
     cantidad= Number(document.getElementById('suma-products-detail').value)
-    document.getElementById('suma-products-detail').value= cantidad += (operator==='+') ? 1 : -1
+    document.getElementById('suma-products-detail').value= cantidad += (operator==='+') ? 1 :-1
 }
